658e02f397e21cef278d094918e45743ab5195de
Vittorio Camisa <vittorio@igenius.net>
2017-12-27 10:12:16 +0100

    Implemented class for parsing and serializing. Implemented PNG

--- imageh/imageh.py
+++ imageh/imageh.py
@@ -1,16 +1,8 @@
+from enum import Enum
+import json
 import re
 import struct
 from typing import List, ByteString, BinaryIO
-from enum import Enum
-
-
-class UnsupportedFormats(Enum):
-    SVG = 'SVG'
-
-
-class SupportedFormats(Enum):
-    PNG = 'PNG'
-    GIF = 'GIF'
 
 
 class ImagehError(Exception):
@@ -25,15 +17,74 @@ class UnsupportedFormatError(ImagehError):
     pass
 
 
+class CustomEncoder(json.JSONEncoder):
+
+    def default(self, o):
+        if isinstance(o, Enum):
+            return o.name
+        return json.JSONEncoder.default(self, o)
+
+
 class ImageDescriptor(object):
 
-    def __init__(self, format: str, width: int, height: int):
+    def __init__(self, format, width, height):
+        # type: (str, int, int) -> None
+
         self.format = format
         self.width = width
         self.height = height
 
+    def json(self):
+        return json.dumps(self.__dict__, cls=CustomEncoder)
+
+
+class PNGColourType(Enum):
+    GREY_SCALE = 0
+    TRUE_COLOUR = 2
+    INDEXED_COLOUR = 3
+    GREY_SCALE_ALPHA = 4
+    TRUE_COLOUR_ALPHA = 6
+
+
+class PNGFilterType(Enum):
+    DEFAULT = 0
+    SUB = 1
+    UP = 2
+    AVERAGE = 3
+    PAETH = 4
+
+
+class PNGCompressionType(Enum):
+    STANDARD = 0
+
+
+class PNGInterlaceType(Enum):
+    NONE = 0
+    ADAM7 = 1
+
+
+class PNGDescriptor(ImageDescriptor):
+
+    def __init__(self,
+                 width,
+                 height,
+                 bit_depth=None,
+                 colour_type=None,
+                 compression=None,
+                 filter_method=None,
+                 interlace_method=None):
+        super(PNGDescriptor, self).__init__('PNG', width, height)
+        self.bit_depth = bit_depth
+        self.colour_type = PNGColourType(colour_type)
+        self.compression = PNGCompressionType(compression)
+        self.filter_method = PNGFilterType(filter_method)
+        self.interlace_method = PNGInterlaceType(interlace_method)
+
 
 class BaseParser(object):
+    """
+    TODO DOC
+    """
 
     def __init__(self, fd):
         # type: (BinaryIO) -> None
@@ -51,15 +102,25 @@ class BaseParser(object):
 
 
 class PNGParser(BaseParser):
+    ihdr_reg = re.compile(b'IHDR(.*)IDAT', flags=re.DOTALL)
 
     @staticmethod
     def check_format(head):
         return bool(re.search(b'PNG', head))
 
     def parse(self):
-        chunk = self.fd.read(24)
-        w, h = map(int, struct.unpack('>LL', chunk[16:24]))
-        return ImageDescriptor(format='PNG', width=w, height=h)
+        """
+        Sources:
+            - https://en.wikipedia.org/wiki/Portable_Network_Graphics
+            - https://www.w3.org/TR/2003/REC-PNG-20031110/
+        """
+        chunk = self.fd.read(42)
+        ihdr = self.ihdr_reg.search(chunk).group(1)
+        # Width, Height, Bit depth, Colour type, Compression method
+        # Filter method, Interlace method
+        values = list(struct.unpack('>LLBBBBB', ihdr[0:13]))
+        desc = PNGDescriptor(*values)
+        return desc
 
 
 class GIFParser(BaseParser):
@@ -74,24 +135,30 @@ class GIFParser(BaseParser):
         return ImageDescriptor(format='GIF', width=w, height=h)
 
 
-def parse(path: str) -> ImageDescriptor:
-    extension = path.split('.')[-1]
-    if getattr(UnsupportedFormats, extension.upper(), None):
-        raise UnsupportedFormatError('Unsupported file type: %s' % extension)
+def parse(path: str):
+    # type: (str) -> ImageDescriptor
+    """
+    TODO DOC
+    :param path: 
+    :return: 
+    """
     with open(path, 'rb') as fd:
         return parse_fd(fd=fd)
 
 
-def parse_fd(fd) -> ImageDescriptor:
-    # 8 bytes is the length of PNG header (GIF has 6 byte header)
-    chunk = fd.read(24)
-    if chunk[:3] == b'GIF':
-        format = SupportedFormats.GIF.value
-        w, h = map(int, struct.unpack('<HH', chunk[6:10]))
-    elif chunk[1:4] == b'PNG':
-        format = SupportedFormats.PNG.value
-        w, h = map(int, struct.unpack('>LL', chunk[16:24]))
+def parse_fd(fd):
+    # type: (BinaryIO) -> ImageDescriptor
+    """
+    TODO DOC
+    :param fd: 
+    :return: 
+    """
+    chunk = fd.read(4)
+    if PNGParser.check_format(chunk):
+        parser = PNGParser(fd)
+    elif GIFParser.check_format(chunk):
+        parser = GIFParser(fd)
     else:
         raise UnsupportedFormatError('Unknown image format')
 
-    return ImageDescriptor(format, w, h)
+    return parser.parse()
